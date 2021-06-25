a74c0b9da229ca233a07c715818127e0804a6ca0
pedrongomes27 <pedro.ngomes27@hotmail.com>
2021-06-25 10:27:52 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculando_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataNasci = Convert.ToDateTime(Console.ReadLine());
+            var idade = DateTime.Today.Year - dataNasci.Year;
+            if (DateTime.Today.DayOfYear < dataNasci.DayOfYear)
+                idade = idade - 1;
+            Console.WriteLine(idade);
+            Console.ReadKey();
+
+        }
+    }
+}
